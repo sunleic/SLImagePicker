1bc80a97e46f6cf32d791939d8fb76ec635df530
SLPowerCoder <sunlei_1030@126.com>
2016-05-03 16:44:14 +0800

    Initial Commit

---
+++ SLImagePicker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SLImagePicker
+//
+//  Created by 孙磊 on 16/5/3.
+//  Copyright © 2016年 孙磊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SLImagePicker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SLImagePicker
+//
+//  Created by 孙磊 on 16/5/3.
+//  Copyright © 2016年 孙磊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
